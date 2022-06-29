950a992fef8202dc6e354833605c746849d6fc26
sahanabise <108336250+sahanabise@users.noreply.github.com>
2022-06-29 17:08:56 +0530

    Update UserLoginScreen.js

--- app/screen/auth/UserLoginScreen.js
+++ app/screen/auth/UserLoginScreen.js
@@ -148,14 +148,22 @@ export default function UserLogin({ navigation }) {
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
-        <View>
-        <TextInput label="Email" returnKeyType="next" value={email.value} onChangeText={(text) => setEmail({ value: text, error: "" })} error={!!email.error} errorText={email.error} autoCapitalize="none" autoCompleteType="email" textContentType="emailAddress" keyboardType="email-address" />
-      <TextInput label="Password" returnKeyType="done" value={password.value} onChangeText={(text) => setPassword({ value: text, error: "" })} error={!!password.error} errorText={password.error} secureTextEntry />
+      <View style={styles.container}>
+      
+        <TextInput label="Email" returnKeyType="next" value={email.value} onChangeText={(text) => setEmail({ value: text, error: "" })} 
+        error={!!email.error} errorText={email.error} 
+        autoCapitalize="none" autoCompleteType="email" 
+        textContentType="emailAddress" keyboardType="email-address" />
+
+      <TextInput label="Password" returnKeyType="done" value={password.value} onChangeText={(text) => setPassword({ value: text, error: "" })} 
+      error={!!password.error} errorText={password.error} secureTextEntry />
       <View style={styles.forgotPassword}>
+
         <TouchableOpacity onPress={() => navigation.navigate("ResetPasswordScreen")}>
           <Text style={styles.forgot}>Forgot your password?</Text>
         </TouchableOpacity>
       </View>
+
       <Button mode="contained" onPress={onLoginPressed} color='#F8772E'>
         Login
       </Button>
@@ -172,6 +180,19 @@ export default function UserLogin({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  container: {
+  fontFamily: 'Segoe UI',
+  fontSize: 55,
+  padding: 20,
+  backgroundColor: '#fff',
+  margin: 30,
+  // marginLeft: 77,
+  // marginRight: 77,
+  // boxShadow: '5px 5px 3px #febe09',
+  // borderRadius: '10px',
+  textAlign: 'center'
+  },
+
   forgotPassword: {
     width: "100%",
     alignItems: "flex-end",
